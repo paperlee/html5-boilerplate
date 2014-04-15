d73e2c534928bad93d91fdb62f389b9339881dea
paperli <paper.li@gmail.com>
2014-04-15 17:48:52 +0800

    Template load cases

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var cases_data = [
 {
 		title:'Qute',
 		article:'<p><a href=\'https://itunes.apple.com/vc/app/qute-qr-code-2d-barcode-scanner/id828181167?mt=8\' target=\'_blank\'>@App Store</a></p><p>Qute is not just the QR code scanner. It also brings the QR code online. People can share and read the comments about the QR code from the world.</p><p>The idea was from the scenarios from street. Image that you saw a QR code on the street. You realized it\'s shit only when you already visit through it. With Qute people can see the comments from the world about the QR code and you decide to continue or abort the visit.</p><p>Beyond that, Qute keeps the scan picture and displays it as part of result. Not only for the visual, but also for the memory. People may recall where the QR code is by the surrending of the QR code in the picture.</p>',
-		bg_url:['../images/qute1.jpg','../images/qute2.jpg','../images/qute3.jpg','../images/qute4.jpg','../images/qute5.jpg'],
+		bg_url:['../images/s/qute1.jpg','../images/s/qute2.jpg','../images/s/qute3.jpg','../images/s/qute4.jpg','../images/s/qute5.jpg'],
 		img_id:'bg_qute',
 		canvas_id:'canvas_qute',
 		base_color:'#b8b0cd',
@@ -12,7 +12,7 @@ var cases_data = [
 		title:'GamerChat',
 		article:'<p><a href=\'https://itunes.apple.com/us/app/gong-hui-tong/id731810260?ls=1&mt=8\' target=\'_blank\'>@App Store</a></p><p>People shall be able to talk at any time, any where, even they are offline of the online game.</p><p>GamerCaht is an app to let Gamers to continue talking on the go. The app inherit the nature of online game: Multi-role for single person, Guild(Group) communication, and Fun-factor passcode. The piano was designed to play the passcode to integrate the joy in practice. The multi-role system was designed to bring gamer\'s experience into chatting.</p>',
 		//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-		bg_url:['../images/gamerchat1.jpg','../images/gamerchat2.jpg','../images/gamerchat3.jpg','../images/gamerchat4.jpg','../images/gamerchat5.jpg'],
+		bg_url:['../images/s/gamerchat1.jpg','../images/s/gamerchat2.jpg','../images/s/gamerchat3.jpg','../images/s/gamerchat4.jpg','../images/s/gamerchat5.jpg'],
 		img_id:'bg_gamerchat',
 		canvas_id:'canvas_gamerchat',
 		base_color:'#b8b0cd',
@@ -22,7 +22,7 @@ var cases_data = [
 			title:'Touchbook',
 			article:'<p>Create your travel memory easily on-the-go!</p><p>The app provided the leasure way to create the memory book with traveling pictures. The inventing instruction and realistic theme are applied in the app.</p>',
 			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-			bg_url:['../images/touchbook2.jpg','../images/touchbook3.jpg','../images/touchbook4.jpg'],
+			bg_url:['../images/s/touchbook2.jpg','../images/s/touchbook3.jpg','../images/s/touchbook4.jpg'],
 			img_id:'bg_touchbook',
 			canvas_id:'canvas_touchbook',
 			base_color:'#b8b0cd',
@@ -32,7 +32,7 @@ var cases_data = [
 				title:'MobiApps',
 				article:'<p><a href=\'https://itunes.apple.com/tw/app/ding-hao/id577065907?mt=8\' target=\'_blank\'>Mobiapps @App Store</a></p><p>In end of 2012, I joined the joyful team MobiApps to lead the UX and UI design. We server the mobile app for retail sale and small business. With rapid design pattern, the mobile app can be prompt built and delivered. The flexibility of the team allowed me to break out serveral concepts including smart no-content mode, social biz UI, etc.. Still, I used variable tools like Dropbox or TAP to rapidly test new concept.</p>',
 				//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-				bg_url:['../images/mobiapps10.jpg','../images/mobiapps8.jpg','../images/mobiapps9.jpg','../images/mobiapps11.jpg','../images/mobiapps12.jpg','../images/mobiapps4.jpg','../images/mobiapps5.jpg'],
+				bg_url:['../images/s/mobiapps10.jpg','../images/s/mobiapps8.jpg','../images/s/mobiapps9.jpg','../images/s/mobiapps11.jpg','../images/s/mobiapps12.jpg','../images/s/mobiapps4.jpg','../images/s/mobiapps5.jpg'],
 				img_id:'bg_mobiapps_app',
 				canvas_id:'canvas_mobiapps_app',
 				base_color:'#b8b0cd',
@@ -42,7 +42,7 @@ var cases_data = [
 		title:'Pilotfish MCBW event',
 		article:'<p><a href=\'http://www.pilotfish.eu/mcbw/\' target=\'_blank\'>http://www.pilotfish.eu/mcbw/</a></p><p>The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. <b>It\'s all about time!</b> The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.</p>',
 		//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-		bg_url:['../images/mcbw-1.jpg','../images/mcbw-2.jpg'],
+		bg_url:['../images/s/mcbw-1.jpg','../images/s/mcbw-2.jpg'],
 		img_id:'bg_mcbw',
 		canvas_id:'canvas_mcbw',
 		base_color:'#b8b0cd',
@@ -52,7 +52,7 @@ var cases_data = [
 			title:'Pilotfish Mobile Website',
 			article:'<p><a href=\'http://www.pilotfish.eu/\' target=\'_blank\'>http://www.pilotfish.eu/</a></p><p>Pilotfish goes to mobile!</p><p>Pilotfish was reforming official website to meet fashion web spec, aka. no flash, go mobile. I was in the team to redesign the flow/wireframe of new offical website (include desktop and mobile version). I made bunch of demo units in HTML to present flow, transition and movement to developer outsourcing.</p><p>The website was announced in end 2012.</p>',
 			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-			bg_url:['../images/pf_mobile1.jpg','../images/pf_mobile5.jpg','../images/pf_mobile6.jpg','../images/pf_mobile8.jpg','../images/pf_mobile4.jpg'],
+			bg_url:['../images/s/pf_mobile1.jpg','../images/s/pf_mobile5.jpg','../images/s/pf_mobile6.jpg','../images/s/pf_mobile8.jpg','../images/s/pf_mobile4.jpg'],
 			img_id:'bg_pf_mobile',
 			canvas_id:'canvas_pf_mobile',
 			base_color:'#b8b0cd',
@@ -63,7 +63,7 @@ var cases_data = [
 			title:'MobiApps Website',
 			article:'<p><a href=\'http://www.themobiapps.com\' target=\'_blank\'>http://www.themobiapps.com/</a></p><p>TheMobiApps, the mobile app start-up in Taiwan, was planning to build the company identify prior to media publishing. I designed the whole new website with the 4 key colors in the company logo. The website consisted of the touch keys in present design trend, one-page scroll, simple and html5. It is planned to publish in March, 2013.</p>',
 			//article:'The big innovation event in Munich! Munich Creative Business Week. I designed a webpage to present speech schedule in Pilotfish booth. The main part of this design is the time-line and the blobs of speakers. As requesed also made the pdf version and invitation card.',
-			bg_url:['../images/mobiapps_web1.jpg','../images/mobiapps_web2.jpg','../images/mobiapps_web3.jpg'],
+			bg_url:['../images/s/mobiapps_web1.jpg','../images/s/mobiapps_web2.jpg','../images/s/mobiapps_web3.jpg'],
 			img_id:'bg_mobiapps',
 			canvas_id:'canvas_mobiapps',
 			base_color:'#b8b0cd',
@@ -72,7 +72,7 @@ var cases_data = [
 		{
 				title:'DDD',
 				article:'<p>We count for Diabetics!</p><p>Diabetics count after dining for blood sugar balance. It may be simple counting for adults, but for kid it\'s the problem.</p><p>I was joining the team in pilotfish for solving this matter. The project contained two parts: iPhone Case with weight scale and the app to count the sugar contained. I made the wired scale for food weight and passed in cloud. The app count the sugar quantity based on online database and the food weight in cloud. The demo kit proved the concept and gain the success in the in-house design competition.</p>',
-				bg_url:['../images/ddd3.jpg','../images/ddd2.jpg','../images/ddd1.jpg','../images/ddd_3.jpg'],
+				bg_url:['../images/s/ddd3.jpg','../images/s/ddd2.jpg','../images/s/ddd1.jpg','../images/s/ddd_3.jpg'],
 				img_id:'bg_ddd',
 				canvas_id:'canvas_ddd',
 				base_color:'#b8b0cd',
@@ -210,6 +210,28 @@ $(document).ready(function(){
 	pics[0] = $('#floor1_box>.pics_box');
 	pics[0].swipe( swipeOptions );
 	
+	// Assign case data
+	for (var key in cases_data){
+		$("body").loadTemplate($("#template"),cases_data[key],{append:true});
+		
+		//set current viewing image at
+		current_img.push(0);
+	}
+	
+	// Apply action in each case
+	$("div.case_whole_box").each(function(key,element){
+		//alert($(element).find("div.pics_box").length);
+		// assign pcis
+		var pics = cases_data[key]["bg_url"];
+		var id_base = cases_data[key]["img_id"];
+		var pic;
+		$(element).find("div.pics_box").width(IMG_WIDTH*pics.length);
+		for (var i in pics){
+			pic = $("<img />").addClass("case_pic").attr("id",id_base+i).attr("src",pics[i]);
+			$(element).find("div.pics_box").append(pic);
+		}
+		
+	});
 	
 });
 
